af8eca710c69f8c82afebd8cd826130dcdd30c97
y-dubovitsky <pXmJ5M9dvP>
2023-03-25 10:23:09 +0300

    Webpack and basic configuration for it added

---
+++ HELP.md
@@ -0,0 +1,3 @@
+## How to configure SCSS modules for Webpack
+
+https://www.developerhandbook.com/blog/webpack/how-to-configure-scss-modules-for-webpack/
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isDevelopment = process.env.NODE_ENV === "development";
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    filename: 'main.[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    hot: true
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html")
+    }),
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+      chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|jpe?g|gif|jpg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.module\.s(a|c)ss$/,
+        use: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              sourceMap: isDevelopment
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        use: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      },
+    ],
+  },
+};
